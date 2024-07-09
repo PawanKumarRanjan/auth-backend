51696fe66349f77a7b91ce9623458af04e0e157c
Pawan Kumar Ranjan <145214435+PawanKumarRanjan@users.noreply.github.com>
2024-07-09 08:26:58 +0530

    Update authRouter.js

--- Routes/authRouter.js
+++ Routes/authRouter.js
@@ -3,7 +3,7 @@ const { signup, login } = require('../Controllers/authController');
 const express = require('express');
 const router = express.Router();
 
-router.post('/auth/login', loginValidation, login);
-router.post('/auth/signup', signupValidation, signup);
+router.post('/login', loginValidation, login);
+router.post('/signup', signupValidation, signup);
 
 module.exports = router;
