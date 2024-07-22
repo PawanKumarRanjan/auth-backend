c453ee91b13eb2617668f1a777c06ebb9b50713b
Pawan Kumar Ranjan <145214435+PawanKumarRanjan@users.noreply.github.com>
2024-07-22 15:48:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const AuthRouter = require('./Routes/authRouter');
 const DashboardRouter = require('./Routes/dashboardRouter')
+const { signupValidation, loginValidation } = require('../Middlewares/authValidation');
+const { signup, login } = require('../Controllers/authController');
+const ensureAuthenticated = require('../Middlewares/auth');
 require('dotenv').config()
 require('./Models/db');
 const PORT = process.env.PORT || 8080;
@@ -19,6 +22,19 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use('/auth', AuthRouter);
 app.use('/dashboard', DashboardRouter);
+
+app.get('/login', (req, res) => {
+    res.send("hello");
+});
+
+app.post('/signup', signupValidation, signup);
+
+app.get('/' ,ensureAuthenticated, (req, res) => {
+    console.log('--- logged in user detail ---');
+    res.status(200).json({
+        message: "hello user"
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
